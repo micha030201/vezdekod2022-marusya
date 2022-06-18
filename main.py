0f3160127cc8a41162a42ac7c172f933e42974b0
micha <micha030201@gmail.com>
2022-06-18 14:09:26 +1000

    first task

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from typing import Union, Dict, Any
+from typing import Union, Dict, Any, List
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 
+class Nlu(BaseModel):
+    tokens: List[str]
+
+
+class Request(BaseModel):
+    command: str
+    original_utterance: str
+    type: str
+    payload: Dict[Any, Any]
+    nlu: Nlu
+
+
 class Item(BaseModel):
     meta: Dict[Any, Any]
-    request: Dict[Any, Any]
+    request: Request
     session: Dict[Any, Any]
     version: str
 
@@ -14,11 +26,17 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post('/')
+@app.post('/marusya')
 async def read_root(req: Item):
+    tokens = req.request.nlu.tokens
+    if 'soft squad' in req.request.command and 'вездекод' in tokens:
+        resp = 'Привет вездекодерам!'
+    else:
+        resp = 'Фу, уходи.'
+
     return {
         'response': {
-            'text': 'sdfhsfjsrgm',
+            'text': resp,
             'end_session': False
         },
         'session': req.session,
