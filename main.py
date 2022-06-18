b1edf01d6ccfd0775a211cf349635c9cb93ca788
micha <micha030201@gmail.com>
2022-06-18 14:36:45 +1000

    exception handling

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import logging
 from typing import Union, Dict, Any, List
 
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
+
+
+logger = logging.getLogger('uvicorn.error')
+logger.setLevel(logging.DEBUG)
 
 
 class Nlu(BaseModel):
@@ -24,6 +31,12 @@ class Item(BaseModel):
 
 
 app = FastAPI()
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    logger.error(str(exc))
+    return PlainTextResponse('', status_code=400)
 
 
 @app.post('/marusya')
